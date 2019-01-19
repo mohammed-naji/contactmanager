cd5ffe7d95b0661c7c5adae159f3fc86a7678046
Mohammed Naji <m.naji1512@gmail.com>
2019-01-19 21:22:41 +0200

    Updates

---
+++ precache-manifest.3f187eb4cbabf52b97e36e26dc707ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "219a790f6cc9d683bd05",
+    "url": "/contactmanager/static/css/main.aef4759f.chunk.css"
+  },
+  {
+    "revision": "219a790f6cc9d683bd05",
+    "url": "/contactmanager/static/js/main.219a790f.chunk.js"
+  },
+  {
+    "revision": "62c0d7ce7663e8029848",
+    "url": "/contactmanager/static/css/1.2567bc29.chunk.css"
+  },
+  {
+    "revision": "62c0d7ce7663e8029848",
+    "url": "/contactmanager/static/js/1.62c0d7ce.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "afe56effa8d91a849bb9cb9428c81d19",
+    "url": "/contactmanager/index.html"
+  }
+];
